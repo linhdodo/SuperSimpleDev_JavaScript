8f8a1c255b80d4eff2a4f61a19fa603c1cd543d8
linhdodo <linhkhanhdo97@yahoo.com>
2023-08-14 21:56:35 -0700

    day19.2

--- Lecture/scripts/12-todo-list.js
+++ Lecture/scripts/12-todo-list.js
@@ -11,7 +11,7 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = '';
 
-  todoList.forEach(function(todoObject, index) {
+  todoList.forEach((todoObject, index) => {
     const { name, dueDate } = todoObject;
     const html = `
       <div>
